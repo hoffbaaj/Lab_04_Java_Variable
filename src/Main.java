3cb5b530ba4e450f1d4788e9de1c4663d96a0d2c
hoffbaaj <hoffbaaj@mail.uc.edu>
2022-09-15 13:39:45 -0400

    Commit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,43 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        int intOperandA = 1;
+        int intOperandB = 2;
+        double doubleOperandA = 1;
+        double doubleOperandB = 2;
+
+
+        int intSum = intOperandA + intOperandB;
+        System.out.println("The sum of " + intOperandA + " and " + intOperandB + " is " + intSum);
+
+        int intProduct = intOperandA * intOperandB;
+        System.out.println("The sum of " + intOperandA + " * " + intOperandB + " is " + intProduct);
+
+        int intDifference = intOperandB - intOperandA;
+        System.out.println("The sum of " + intOperandB + " - " + intOperandA + " is " + intDifference);
+
+        int intQuotient = intOperandB / intOperandA;
+        System.out.println("The sum of " + intOperandB + " divided by " + intOperandA + " is " + intQuotient);
+
+        int intModulo = intOperandA % intOperandB;
+        System.out.println("The sum of " + intOperandA % intOperandB + " is " + intModulo);
+
+
+        double doubleSum = doubleOperandA + doubleOperandB;
+        System.out.println("The sum of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleSum);
+
+        double doubleProduct = doubleOperandA * doubleOperandB;
+        System.out.println("The sum of " + doubleOperandA + " * " + doubleOperandB + " is " + doubleProduct);
+
+        double doubleDifference = doubleOperandA - doubleOperandB;
+        System.out.println("The sum of " + doubleOperandA + " - " + doubleOperandB + " is " + doubleDifference);
+
+        double doubleQuotient = doubleOperandA / doubleOperandB;
+        System.out.println("The sum of " + doubleOperandA + " divided by " + doubleOperandB + " is " + doubleQuotient);
+
+        double doubleModulo = doubleOperandA % doubleOperandB;
+        System.out.println("The sum of " + doubleOperandA % doubleOperandB + " is " + doubleModulo);
+
+
     }
 }
